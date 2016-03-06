673f255c8d682cb2ddfedd59fc4845c0dc594aec
Broenne <marcus.broenstrup@gmail.com>
2016-03-06 22:59:56 +0100

    no message

--- MeterImage/MeterImageTeaching/MeterImageTeaching.py
+++ MeterImage/MeterImageTeaching/MeterImageTeaching.py
@@ -2,10 +2,13 @@
 #http://docs.opencv.org/3.1.0/d5/de5/tutorial_py_setup_in_windows.html#gsc.tab=0
 import cv2
 import numpy as np
-from MeterImageTeaching import cv2
+import os
 
 print "This line will be printed."
+path = os.getcwd()
+print path
 
 
-
-img = cv2.imread('C:\apps\OcrOpenCvNET\MeterImage\MeterImage\BilderMartin\pic2\Snipet_WP_20160226_017.jpg')
+img = cv2.imread('C:/apps/OcrOpenCvNET/MeterImage/MeterImage/BilderMartin/pic2/Snipet_WP_20160226_017.jpg')
+cv2.imshow('ImageWindow',img)
+cv2.waitKey(0)
